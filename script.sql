6d92bb46821846140bb8919aa70418d5a80c963d
17vle3 <17vle3@gmail.com>
2021-02-18 11:36:22 -0800

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,5 +1,18 @@
-CREATE TABLE employees ( SSN  CHAR(11) NOT NULL,  salary  float,  phone  numeric ( 10 , 0 ),  PRIMARY KEY (SSN) ); 
-CREATE TABLE departments ( dno  INTEGER NOT NULL,  budget  float,  dname text,  PRIMARY KEY (dno) );
-CREATE TABLE works ( SSN CHAR(11) NOT NULL,  dno INTEGER NOT NULL,  PRIMARY KEY (SSN, dno),  FOREIGN KEY (SSN)  REFERENCES employees ,  FOREIGN KEY (dno)  REFERENCES departments);
-CREATE TABLE Manages (  SSN  CHAR(11) NOT NULL,  dno INTEGER NOT NULL,  PRIMARY KEY (dno),  FOREIGN KEY (SSN)  REFERENCES employees ,  FOREIGN KEY (dno)  REFERENCES departments);   
-CREATE TABLE children ( SSN CHAR(11) NOT NULL, name text NOT NULL,  PRIMARY KEY (SSN, name),  FOREIGN KEY (SSN)  REFERENCES employees ON DELETE CASCADE);
+SELECT pid
+FROM catalog
+WHERE cost < 10
+
+SELECT pname
+FROM catalog, parts
+WHERE cost < 10
+
+Select address 
+FROM suppliers 
+WHERE sname = “Fire Hydrant Cap”
+
+Select sname 
+FROM catalog, parts,suppliers
+WHERE color = “green”
+
+Select sname,pname
+FROM catalog, parts,suppliers
