3177685e0f14f782668c50ba70b8573efd4ba0f6
Vivian Le <vle018@wch132-05.cs.ucr.edu>
2021-02-18 11:50:20 -0800

    :D

--- script.sql
+++ script.sql
@@ -1,18 +1,7 @@
 SELECT pid
 FROM catalog
-WHERE cost < 10
+WHERE cost < 10;
 
-SELECT pname
-FROM catalog, parts
-WHERE cost < 10
-
-Select address 
-FROM suppliers 
-WHERE sname = “Fire Hydrant Cap”
-
-Select sname 
-FROM catalog, parts,suppliers
-WHERE color = “green”
-
-Select sname,pname
-FROM catalog, parts,suppliers
+SELECT parts.pname
+FROM catalog,parts
+WHERE cost<10
