1c8984be4c75ab8fb653b337a01ab224d391f8a8
Vivian Le <vle018@wch132-05.cs.ucr.edu>
2021-02-18 11:57:13 -0800

    :D

--- script.sql
+++ script.sql
@@ -4,4 +4,16 @@ WHERE cost < 10;
 
 SELECT parts.pname
 FROM catalog,parts
-WHERE cost<10
+WHERE cost<10;
+
+Select address 
+FROM suppliers 
+WHERE suppliers.sname = 'Fire Hydrant Cap';
+
+Select sname 
+FROM catalog, parts,suppliers
+WHERE parts.color = 'Green';
+
+Select sname,pname
+FROM catalog, parts,suppliers;
+
