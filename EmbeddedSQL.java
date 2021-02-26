348817a998764bacb904d60dc76e4c7618750bbc
17vle3 <17vle3@gmail.com>
2021-02-25 19:13:08 -0800

    Update EmbeddedSQL.java

--- EmbeddedSQL.java
+++ EmbeddedSQL.java
@@ -310,7 +310,7 @@ public class EmbeddedSQL {
    public static void Query6(EmbeddedSQL esql){
       try{
          String query = "SELECT address FROM catalog C,parts P,suppliers S WHERE S.sid = C.sid AND P.pid = C.pid AND pname = \'";
-         System.out.print("\tEnter cost: $");
+         System.out.print("\tEnter pname: $");
          String input = in.readLine();
          query += input;
 	 query+="\'";
