2e9d93d7f20a73106d89da287e3d652beb3a305e
17vle3 <17vle3@gmail.com>
2021-02-25 16:49:53 -0800

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,19 +1,31 @@
-SELECT pid
-FROM catalog
-WHERE cost < 10;
+SELECT S.sname,COUNT(*) as PartCount
+FROM Suppliers S, Catalog C, Parts P
+WHERE C.sid = S.sid and P.pid = C.pid
+GROUP BY  S.sname, S.id;
 
-SELECT parts.pname
-FROM catalog,parts
-WHERE cost<10;
+SELECT S.sname,COUNT(*) as PartCount
+FROM Suppliers S, Catalog C, Parts P
+WHERE C.sid = S.sid and P.pid = C.pid
+GROUP BY  S.sname, S.id
+HAVING COUNT(pid) <=3;
 
-Select address 
-FROM suppliers 
-WHERE suppliers.sname = 'Fire Hydrant Cap';
+SELECT S.sname, COUNT(*) as PartCount
+FROM Suppliers S, Catalog C, Parts P
+WHERE C.sid = S.id and P.id = C.pid and P.color like 'Green%'
+GROUP BY S.sname, S.id;
 
-Select sname 
-FROM catalog, parts,suppliers
-WHERE parts.color = 'Green';
+create view temp
+as
 
-Select sname,pname
-FROM catalog, parts,suppliers;
+SELECT DISTINCT C.sid
+FROM Catalog C, Parts P
+WHERE P.id = C.pid and P.color like 'Red%'
+INTERSECT
+SELECT DISTINCT C1.sid 
+FROM Suppliers S1, Catalog C1, Parts P1
+WHERE P1.id = C1.pid and P.color like 'Green%';
 
+SELECT S.sname,Max(C.cost)
+from temp,Catalog C,suppliers S
+where temp.sid=C.sid and C.sid=S.id
+GROUP BY S.sname, S.id;
