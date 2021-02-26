06049c734e2e4c2266d5af8936e8bde30ecef162
17vle3 <17vle3@gmail.com>
2021-02-25 18:54:30 -0800

    Update script.sql

--- script.sql
+++ script.sql
@@ -15,11 +15,11 @@ WHERE C.sid = S.sid and P.pid = C.pid and P.color like 'Green%'
 GROUP BY S.sname, S.sid;
 
 SELECT DISTINCT S.sname, Max(C.cost)
-FROM Catalog C, Parts P
+FROM Suppliers S, Catalog C, Parts P
 WHERE C.sid=S.sid and P.pid = C.pid and P.color like 'Red%'
 INTERSECT
 SELECT DISTINCT S1.sname, Max(C1.cost), 
 FROM Suppliers S1, Catalog C1, Parts P1
-WHERE C.sid=S.sid and P1.id = C1.pid and P1.color like 'Green%';
+WHERE C1.sid=S1.sid and P1.id = C1.pid and P1.color like 'Green%';
 
 
