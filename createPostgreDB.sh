cc95fc16d99458009dfdc2e85af01825db69cacc
17vle3 <17vle3@gmail.com>
2021-03-04 19:08:57 -0800

    Add files via upload

--- createPostgreDB.sh
+++ createPostgreDB.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-echo "creating db named ... "$USER"_DB"
-createdb -h localhost -p $PGPORT $USER"_DB"
+export DB_NAME=$USER"_DB"
+echo "creating db named ... "$DB_NAME
+createdb -h localhost -p $PGPORT $DB_NAME
 pg_ctl status
+
+cp *.dat $PGDATA/
