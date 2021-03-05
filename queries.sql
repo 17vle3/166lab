ac213626ab8416bb9fce2bea9cdd032054969b0e
17vle3 <17vle3@gmail.com>
2021-03-05 13:40:36 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,7 +11,7 @@ FROM part_nyc n , part_sfo s
 WHERE  n.color = 0  and s.color= 0;
 =======*/
 
-select (select count(*) from part_nyc where color = 'Red') + (select count(*) from part_nyc where color = 'Red') from part_nyc;
+select (select count(*) from part_nyc where color = 0) + (select count(*) from part_nyc where color = 0) from part_nyc;
 
 
 /*3. List all the suppliers that have more total on hand parts in NYC than
