f52ed7eea326fd1fbd835f09200975c95a739344
17vle3 <17vle3@gmail.com>
2021-03-05 18:32:11 -0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -11,14 +11,27 @@ FROM part_nyc n , part_sfo s
 WHERE  n.color = 0  and s.color= 0;
 =======*/
 
-select COALESCE((select SUM(on_hand)  from part_nyc where color = 0),0) + COALESCE((select SUM(on_hand) from part_nyc where color = 0),0) ;
+select COALESCE((select SUM(n.on_hand)  from part_nyc n, color c where N.color = C.color_id AND color_name = 'Red'),0) + COALESCE((select SUM(s.on_hand) from part_sfo s,color cwhere N.color = C.color_id AND color_name = 'Red'),0) AS numTwo ;
 
 /*3. List all the suppliers that have more total on hand parts in NYC than
 they do in SFO.*/
 
-SELECT  DISTINCT n.supplier 
+/*SELECT  DISTINCT n.supplier 
 FROM part_nyc n , part_sfo s
 WHERE  (select SUM(n1.on_hand)  from part_nyc n1 where n1.supplier = n.supplier) > (select SUM(s2.on_hand)  from part_sfo s2 where s.supplier = s2.supplier);
+*/
+
+SELECT S.supplier_id, S.supplier_name
+FROM supplier S
+WHERE
+(SELECT SUM(NYC.on_hand)
+FROM part_nyc NYC
+WHERE S.supplier_id = NYC.supplier)
+>
+(SELECT SUM(SFO.on_hand)
+FROM part_sfo SFO
+WHERE S.supplier_id = SFO.supplier)
+ORDER BY S.supplier_id;
 
 
 /*4. List all suppliers that supply parts in NYC that aren’t supplied by
