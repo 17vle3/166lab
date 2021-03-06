d873761389a2752db58a65ed504beaef0d5f8b44
17vle3 <17vle3@gmail.com>
2021-03-05 18:22:17 -0800

    Update create_indexes.sql

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,45 +1,30 @@
-/*1.Count how many parts in NYC have more than 70 parts on hand*/
-CREATE INDEX numOne ON part_nyc(on_hand) WHERE on_hand>70;
+/*btree is the default*/
+CREATE INDEX supID
+ON supplier (supplier_id);
 
+CREATE INDEX supName
+ON supplier (supplier_name);
 
-begin
-    dbms_stats.gather_table_stats(user, 'part_nyc');
-end;
+CREATE INDEX nycPartNum
+ON part_nyc (part_number);
 
-/*2. Count how many total parts on hand, in both NYC and SFO, are Red 0 = red 1 = green*/
+CREATE INDEX nycSup
+ON part_nyc (supplier);
 
-CREATE INDEX numTwo
-ON part_nyc(on_hand)
-WHERE color = 'Red';
+CREATE INDEX nycColor
+ON part_nyc (color);
 
-CREATE INDEX numTwo
-ON part_sfo(on_hand)
-WHERE color = 'Red';
+CREATE INDEX nycOnHand
+ON part_nyc (on_hand);
 
-begin
-    dbms_stats.gather_table_stats(user, 'part_nyc');
-    dbms_stats.gather_table_stats(user, 'part_sfo');
-end;
+CREATE INDEX sfoPartNum
+ON part_sfo (part_number);
 
+CREATE INDEX sfoSup
+ON part_sfo (supplier);
 
-/*3. List all the suppliers that have more total on hand parts in NYC than
-they do in SFO.*/
+CREATE INDEX sfoColor
+ON part_sfo (color);
 
-SELECT  n.supplier 
-FROM part_nyc n , part_sfo s
-WHERE  n.on_hand>s.on_hand;
-
-/*4. List all suppliers that supply parts in NYC that aren’t supplied by
-anyone in SFO.*/
-
-SELECT  n.supplier 
-FROM part_nyc n , part_sfo s
-WHERE  NOT EXISTS(n.part_number= s.part_number);
-
-/*5. Update all of the NYC on hand values to on hand - 10.*/
-
-UPDATE part_nyc 
-SET on_hand = 10;
-
-/*6. Delete all parts from NYC which have less than 30 parts on hand.*/
-DELETE FROM part_nyc  WHERE on_hand<30;
+CREATE INDEX sfoOnHand
+ON part_sfo (on_hand);
