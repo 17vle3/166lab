e7fe629cb1d11ce0ac79e2b722375747d3003f7e
17vle3 <17vle3@gmail.com>
2021-03-05 18:23:40 -0800

    Update create_indexes.sql

--- create_indexes.sql
+++ create_indexes.sql
@@ -1,30 +1,15 @@
 /*btree is the default*/
-CREATE INDEX supID
-ON supplier (supplier_id);
 
-CREATE INDEX supName
-ON supplier (supplier_name);
 
-CREATE INDEX nycPartNum
-ON part_nyc (part_number);
+CREATE INDEX nycPartNum ON part_nyc (part_number);
+CREATE INDEX nycSup ON part_nyc (supplier);
+CREATE INDEX nycColor ON part_nyc (color);
+CREATE INDEX nycOnHand ON part_nyc (on_hand);
 
-CREATE INDEX nycSup
-ON part_nyc (supplier);
+CREATE INDEX sfoPartNum ON part_sfo (part_number);
+CREATE INDEX sfoSup ON part_sfo (supplier);
+CREATE INDEX sfoColor ON part_sfo (color);
+CREATE INDEX sfoOnHand ON part_sfo (on_hand);
 
-CREATE INDEX nycColor
-ON part_nyc (color);
-
-CREATE INDEX nycOnHand
-ON part_nyc (on_hand);
-
-CREATE INDEX sfoPartNum
-ON part_sfo (part_number);
-
-CREATE INDEX sfoSup
-ON part_sfo (supplier);
-
-CREATE INDEX sfoColor
-ON part_sfo (color);
-
-CREATE INDEX sfoOnHand
-ON part_sfo (on_hand);
+CREATE INDEX supID ON supplier (supplier_id);
+CREATE INDEX supName ON supplier (supplier_name);
